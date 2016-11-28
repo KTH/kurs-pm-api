2733e28dd5a17c9306fe94759406e823ac8f9ca9
Jonas Lantto <jonas.e.lantto@gmail.com>
2016-11-28 15:42:20 +0100

    using the new improved kth-node-mongo

--- server/init/database/index.js
+++ server/init/database/index.js
@@ -3,45 +3,20 @@
 const config = require('../configuration')
 const log = require('kth-node-log')
 const nodeMongo = require('kth-node-mongo')
-const fs = require('fs')
-const path = require('path')
-
-let certs
-let sslOptions
-let dbUri = config.secure.db.uri
-
-if (!dbUri && config.full.db) {
-  dbUri = config.full.db.uri
-}
-
-let useSsl = config.secure.db.ssl
-
-if (useSsl) {
-  if (Array.isArray(config.secure.db.caCerts)) {
-    certs = config.secure.db.caCerts.map(cert => {
-      if (cert.indexOf('/') === 0) {
-        return fs.readFileSync(cert)
-      }
-
-      return fs.readFileSync(path.join(__dirname, '/../../../config/', cert))
-    })
-  }
-
-  sslOptions = {
-    ssl: useSsl,
-    authenticationDatabase: config.secure.db.authDatabase,
-    sslCA: certs
-  }
-}
 
 const mongoOptions = {
   dbUsername: config.secure.db.username,
   dbPassword: config.secure.db.password,
-  dbUri: dbUri,
+  server: {
+    authenticationDatabase: config.secure.db.authDatabase,
+    ssl: config.secure.db.ssl
+  },
+  maxPoolSize: 5,
+  dbUri: config.secure.db.uri,
   logger: log
 }
 
-nodeMongo.connect(mongoOptions, sslOptions)
+nodeMongo.connect(mongoOptions)
   .then(data => {
     log.info({ data: data }, 'MongoDB: connected')
   })
