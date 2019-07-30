33bb0bfcca72cd0e1fe2fd5d0b641a6465da5fce
Elena Rakhimova <elenara@kth.se>
2019-07-30 15:02:37 +0200

    Added paths for get,post,put,delete course pm data by id

--- server/server.js
+++ server/server.js
@@ -122,16 +122,18 @@ addPaths('api', createApiPaths({
 const authByApiKey = passport.authenticate('apikey', { session: false })
 
 // Application specific API enpoints
-const { Sample } = require('./controllers')
+const { CourseMemoDocumentCtrl } = require('./controllers')
 const ApiRouter = require('kth-node-express-routing').ApiRouter
 const apiRoute = ApiRouter(authByApiKey)
 const paths = getPaths()
 
 // Api enpoints
 apiRoute.register(paths.api.checkAPIkey, System.checkAPIKey)
+apiRoute.register(paths.api.getCourseMemoDocumentDataById, CourseMemoDocumentCtrl.getDataById)
+apiRoute.register(paths.api.postCourseMemoDocumentDataById, CourseMemoDocumentCtrl.posttDataById)
+apiRoute.register(paths.api.putCourseMemoDocumentDataById, CourseMemoDocumentCtrl.putDataById)
+apiRoute.register(paths.api.deleteCourseMemoDocumentDataById, CourseMemoDocumentCtrl.deleteDataById)
 
-apiRoute.register(paths.api.getDataById, Sample.getData)
-apiRoute.register(paths.api.postDataById, Sample.postData)
 server.use('/', apiRoute.getRouter())
 
 // Catch not found and errors
