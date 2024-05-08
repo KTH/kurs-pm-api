d36cec653e3c8b56c2faa7c5f8029a922317768a
Karl Andin <karl.andin@drakryggen.com>
2024-05-08 10:59:59 +0200

    feat(KUI-1306): add application insights

--- server/server.js
+++ server/server.js
@@ -1,16 +1,13 @@
 'use strict'
 const server = require('@kth/server')
 const path = require('path')
+
 // Load .env file in development mode
 const nodeEnv = process.env.NODE_ENV && process.env.NODE_ENV.toLowerCase()
 if (nodeEnv === 'development' || nodeEnv === 'dev' || !nodeEnv) {
   require('dotenv').config()
-} else if (!process.env.SERVICE_PUBLISH) {
-  // This is an ANSIBLE machine which doesn't set env-vars atm
-  // so read localSettings.js which we now use to fake env-vars
-  // because it already exists in our Ansible setup.
-  require('../config/localSettings')
 }
+
 // Now read the server config etc.
 const config = require('./configuration').server
 const AppRouter = require('kth-node-express-routing').PageRouter
